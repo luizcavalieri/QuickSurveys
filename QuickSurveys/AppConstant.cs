b9436aa35ecf6957edeee9e470ca6c1278450a0d
luizcavalieri <luiz.cavalieri@gmail.com>
2016-05-01 11:28:23 +1000

    SURVEY-25 Creting Question Class and DataSets for questions

--- QuickSurveys/AppConstant.cs
+++ QuickSurveys/AppConstant.cs
@@ -6,9 +6,9 @@ using System.Web;
 namespace QuickSurveys
 {
     public class AppConstant
-    {
-        public static String DevConnectionString = "Data Source=SQL5025.myWindowsHosting.com;Initial Catalog=DB_9AB8B7_DDA5080;User Id=DB_9AB8B7_DDA5080_admin;Password=hG3Qu25k";
-        public static String TestConnectionString = "Data Source=SQL5006.myWindowsHosting.com;Initial Catalog=DB_9AB8B7_enterprise;User Id=DB_9AB8B7_enterprise_admin;Password=enterprise";
-        public static String ProdConnectionString = "";
+    {        
+        public static String DevConnectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=DB_9AB8B7_DDA5080;Integrated Security=True";
+        public static String TestConnectionString = "Data Source=SQL5025.myWindowsHosting.com;Initial Catalog=DB_9AB8B7_DDA5080;User Id=DB_9AB8B7_DDA5080_admin;Password=hG3Qu25k";
+        public static String ProdConnectionString = "Data Source=SQL5006.myWindowsHosting.com;Initial Catalog=DB_9AB8B7_enterprise;User Id=DB_9AB8B7_enterprise_admin;Password=enterprise";
     }
 }
---
+++ QuickSurveys/BusinessLogic/Question.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace QuickSurveys
+{
+    public class Question
+    {
+        #region Fields
+
+        private int _quest_id;
+        private string _quest_description;
+        private int _quest_answer_group_option_id;
+        private int _quest_required;
+        private int _quest_input_type_id;
+        private int _quest_survey_id;
+        private int _quest_survey_sequence;
+        private int _quest_main_id;
+        private int _quest_child_sequence;
+
+        #endregion
+
+        #region Properties
+
+        public int quest_id
+        {
+            get { return _quest_id;  }
+            set { _quest_id = value; }
+        }
+
+        public string quest_description
+        {
+            get { return _quest_description; }
+            set { _quest_description = value; }
+        }
+        public int quest_answer_group_option_id
+        {
+            get { return _quest_answer_group_option_id; }
+            set { _quest_answer_group_option_id = value; }
+        }
+        public int quest_required
+        {
+            get { return _quest_required; }
+            set { _quest_required = value; }
+        }
+        public int quest_input_type_id
+        {
+            get { return _quest_input_type_id; }
+            set { _quest_input_type_id = value; }
+        }
+        public int quest_survey_id
+        {
+            get { return _quest_survey_id; }
+            set { _quest_survey_id = value; }
+        }
+        public int quest_survey_sequence
+        {
+            get { return _quest_survey_sequence; }
+            set { _quest_survey_sequence = value; }
+        }
+
+        public int quest_main_id
+        {
+            get { return _quest_main_id; }
+            set { _quest_main_id = value; }
+        }
+
+        public int quest_child_sequence
+        {
+            get { return _quest_child_sequence; }
+            set { _quest_child_sequence = value; }
+        }
+        
+        #endregion
+
+        # region method
+
+        public static TabBookModel Parse(bookDS.TabReservedRow row)
+        {
+
+        }
+
+        #endregion
+
+    }
+}
